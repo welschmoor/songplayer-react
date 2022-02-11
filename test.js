bbacc24ca7b913dc96445874425f37b1536913a5
Johannes Welsch <welschmoor@gmail.com>
2022-02-11 16:40:47 +0100

    added preview picture

---
+++ test.js
@@ -0,0 +1,19 @@
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+
+
+    
+
+const lightTheme = {
+  navCol: "#ffffff",
+  bgCol: "hsl(0, 0%, 95%)",
+  cardCol: "#ffffff",
+  textShadow: "none",
+  buttonBg: "#374237;",
+}
+const darkTheme = {
+  navCol: "rgb(40, 40, 46);",
+  bgCol: "rgb(40, 40, 46);",
+  textCol: "#fff;",
+  textShadow: "1px 1px 1px black",
+  buttonBg: "linear-gradient(180deg, #bbdfbb 0%, #8eb68e 100%)",
+}
